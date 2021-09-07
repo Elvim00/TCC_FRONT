63a34759e2c656114cd56d035ada7fdeb846c718
Elvis Monteiro <elvismonteiroamb@gmail.com>
2021-09-07 02:58:26 -0300

    Segundo Comiit

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,36 +1,88 @@
-import React, { UseState } from 'react';
+import React from 'react';
 import './login.css'
 import { InputText } from 'primereact/inputtext';
-import { MdEmail, MdLock, MdVisibility, MdVisibilityOff } from "react-icons/md";
+import { MdEmail, MdLock} from "react-icons/md";
 import { Button } from 'primereact/button';
+import axios from 'axios';
 
-function login (){
-    return (
+var usuario;
+var senha;
+function preencherUsuario(valor){  
+  usuario = valor;
+}
+
+function preencherSenha(valor){
+  senha = valor;
+}
+
+async function ValidaUsuario (){ 
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Login": {usuario},
+    "senha": {senha}
+  };
+  const url = "/ValidarUsuario";
+
+    axios.get(url, {headers})
+    .then((response) => {
+      console.log(response.data[0].usuario_id)
+  //    console.log(event.target.usuario.value)
+     // console.log({senha})
+    //  if (response.data.usuario_id = 1) {
+   //     alert('OK')
+   //   } else {
+   //     alert('Erro')
+   //   }
+      
+    }).catch((error) => {
+        alert("Ocorreu um erro ao buscar os items" + error);
+    });   
+    
+    
+}  
+
+
+function login (){ 
+  
+     
+    return (        
+      
         <div className = "login">
+          
+          
             <div className = "login-logo">
-                <img src = "https://software.muz.ifsuldeminas.edu.br/balanced_diet_v2/assets/img/login.png"/>
-            </div>  
-
+                <img src = "https://software.muz.ifsuldeminas.edu.br/balanced_diet_v2/assets/img/login.png" alt =''/>
+            </div>                        
             <div className = "login-componente"> 
               <h1>Acesso ao sistema</h1>
 
+              
               <div className = "login-usuario">   
                 <MdEmail/>
                 <InputText                   
-                  type = 'text'
-                  placeholder = 'Digite um usuario'                  
+                  type = 'text'                  
+                  placeholder = 'Digite um usuario'  
+                  onKeyDown={(e) => preencherUsuario(e.target.value)}
                 />                    
               </div>   
 
               <div className = "login-senha"> 
                 <MdLock/>
                 <InputText                   
-                    type = 'text'
-                    placeholder = 'Digite uma senha'/> 
+                    type = 'text'                    
+                    placeholder = 'Digite uma senha'
+                    onKeyDown={(e) => preencherSenha(e.target.value)}
+                    />                                      
+                    
               </div>    
              
 
-              <Button type = "submit" icon="pi pi-check"> 
+              <Button 
+              type = "submit" 
+              icon="pi pi-check"
+              onClick={ValidaUsuario}
+              >
                 Entrar
               </Button>   
 
@@ -38,10 +90,11 @@ function login (){
               <Button type = "submit"> 
                 Cadastrar
               </Button>                  
-
+                
             </div>         
+            
         </div>
+        
     )
 }
-
-export default login
+export default login;
