ee7ba4778e1d7eb680427aadcc9179825b1064fa
Felipe Pinto <felipearpinto@gmail.com>
2021-09-21 20:36:47 -0300

    Adicionado rota da tela principal

--- src/Pages/gerenciadorRotas.js
+++ src/Pages/gerenciadorRotas.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Cadastro from './Cadastro/cadastro';
 import TelaLogin from '../Pages/Login/login';
+import Principal from '../Pages/Principal/principal';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
-function CarregarRota(){
+function CarregarRota() {
     return(
-        <BrowserRouter>
-            <Switch>
+            <BrowserRouter>
+                <Switch>
                 <Route path="/" exact component={TelaLogin} />
                 <Route path="/cadastro-usuario" component={Cadastro} />                
-            </Switch>        
-        </BrowserRouter>
-    );
-};
+                <Route path="/tela-principal" component={Principal} />                
+                </Switch>        
+            </BrowserRouter>
+            );
+}
+;
 
 export default CarregarRota;
