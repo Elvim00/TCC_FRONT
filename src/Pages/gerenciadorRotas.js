54b06c06b86afd62f2b6dd7b1023fcce9e4d693d
Elvis Monteiro <elvismonteiroamb@gmail.com>
2021-09-21 20:34:00 -0300

    Adicionando tela Tarefa

--- src/Pages/gerenciadorRotas.js
+++ src/Pages/gerenciadorRotas.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cadastro from './Cadastro/cadastro';
 import TelaLogin from '../Pages/Login/login';
+import Tarefa from '../Pages/Tarefas/Tarefa'
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
@@ -9,7 +10,8 @@ function CarregarRota(){
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={TelaLogin} />
-                <Route path="/cadastro-usuario" component={Cadastro} />                
+                <Route path="/cadastro-usuario" component={Cadastro} />         
+                <Route path="/Tarefa" component={Tarefa}/>
             </Switch>        
         </BrowserRouter>
     );
